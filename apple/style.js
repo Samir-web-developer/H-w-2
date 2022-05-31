6abfe049478948f2ad544c3c3621f08f268d6eaf
Samir-web-developer <106134956+Samir-web-developer@users.noreply.github.com>
2022-05-31 09:52:56 +0500

    Add files via upload

---
+++ apple/style.js
@@ -0,0 +1,61 @@
+/* ------------------------------ */
+// let name = prompt('Введите число')
+
+// if(name % 2 == 0) {
+//     alert('Четное')
+// }else {
+//     alert('Нечетное')
+// }
+
+/* ------------------------------ */
+
+// let text = prompt('Введите имя')
+
+// if(text[0] == 'B' || text[0] == 'b' && text % 2 == 0){
+//     alert('Welcome')
+// }else {
+//     alert('Close')
+// }
+
+/* ------------------------------ */
+
+// let seson = prompt('Введите число')
+
+// if(seson == 12 || seson == 1 || seson == 2 && seson <=12){
+//     alert('Это зима')
+// }else if(seson == 3 || seson == 4 || seson == 5){
+//     alert('Это весна')
+// }else if(seson == 6 || seson == 7  || seson == 8) {
+//     alert('Это лето')
+// }else if(seson == 9 || seson == 10 || seson == 11){
+//     alert('Это осень')
+// }
+// else {
+//     alert('Ошибка')
+// }
+
+/* ------------------------------ */
+
+// let str = prompt('Узнать какая сейчас деката')
+
+// if(str >= 10 && str <=0 && str >= 31) {
+//     alert('Первая декада')
+// }else if (str >= 11 && str <= 20) {
+//     alert('Вторая декада')
+// }else if(str >= 21 && str <= 30) {
+//     alert('Третья декада')
+// }else {
+//     alert('Ошибка')
+// }
+
+/* ------------------------------ */
+
+// let busy = prompt('Сколько занятых мест')
+// let club = prompt('Сколько людей хотят зайти')
+// let have = +busy + +club
+
+// if(have <= 10){
+//     alert('Welcome')
+// }else {
+//     alert('Get out')
+// }
